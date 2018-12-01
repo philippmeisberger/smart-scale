9a2cdbcd9aff03bfb8ed6e025ac01985d30d45ee
Philipp Meisberger <team@pm-codeworks.de>
2018-12-01 15:00:00 +0100

    Introduced switchMode() to switch to a specified mode; Introduced selectMode() to allow user to select another mode

--- src/Firmware/sketches/mode.h
+++ src/Firmware/sketches/mode.h
@@ -17,5 +17,31 @@ int selected_mode = 0;
 inline void addMode(void (*start_mode)(), void (*update)(), char* name) {
   modes[next_mode++] = mode(start_mode, update, name);
 }
-    
+
+void switchMode(int newMode) {
+  selected_mode = min(max(0, newMode), next_mode - 1);
+  DEBUG_PRINTF("switchMode(): Changing mode to %i\n", selected_mode);
+  modes[selected_mode].setup();
+}
+
+void selectMode() {
+  // TODO: Replace by button push
+  if (Serial.available() > 0) {
+    DEBUG_PRINTF("switchMode(): %i modes available\n", next_mode);
+    int selected = selected_mode;
+    char in = Serial.read();
+
+    if (in == 'w') {
+      selected--;
+    }
+    else if (in == 's') {
+      selected++;
+    }
+
+    Serial.flush();
+    switchMode(selected);
+    delay(100);
+  }
+}
+
 #endif //MODE_H
