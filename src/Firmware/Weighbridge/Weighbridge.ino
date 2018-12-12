cff03bd0f4b280e0fd7021a33bdaa85be386ca08
Philipp Meisberger <team@pm-codeworks.de>
2018-12-12 12:12:06 +0100

    Moved MQTT publish into own header file; Moved WiFi handling into main sketch so all modes are able to use it

--- src/Firmware/Weighbridge/Weighbridge.ino
+++ src/Firmware/Weighbridge/Weighbridge.ino
@@ -7,12 +7,18 @@
 #include "mode.h"
 #include "snake.h"
 #include "weighbridge.h"
+#include "mqtt.h"
 
-#define FIRMWARE_VERSION "0.5"
+#define FIRMWARE_VERSION "0.6"
 
 // Connect SCL to D1 and SDA to D2
 #define OLED_RESET 0
 Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, OLED_RESET);
+
+// Timestamp of last Wi-Fi connection attempt
+int lastWifiConnectionAttempt = 0;
+
+extern PubSubClient mqttClient;
 
 // TODO: No menu should be provided
 /*namespace MAIN_MODE{
@@ -80,6 +86,32 @@ void setupDisplay() {
   display.display();
 }
 
+void setupWifi()
+{
+  // Stay offline
+  if ((WIFI_SSID == "") || (WIFI_PASSWORD == ""))
+    return;
+
+  // Already connected
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    DEBUG_PRINT("setupWifi(): Connected to Wi-Fi access point. IP address: ");
+    DEBUG_PRINTLN(WiFi.localIP());
+    return;
+  }
+
+  DEBUG_PRINTF("setupWifi(): Connecting to Wi-Fi access point '%s'\n", WIFI_SSID);
+
+  // Do not store Wi-Fi config in SDK flash area
+  WiFi.persistent(false);
+
+  // Disable auto Wi-Fi access point mode
+  WiFi.mode(WIFI_STA);
+
+  // Start Wi-Fi connection
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
 void setup() {
 #ifdef DEBUG
   Serial.begin(115200);
@@ -87,6 +119,7 @@ void setup() {
   Serial.printf("ESP8266 Smart Weighbridge '%s'\n", FIRMWARE_VERSION);
 #endif
   setupDisplay();
+  setupWifi();
   // randomSeed(analogRead(0));
 
   // Include all modes
@@ -97,4 +130,13 @@ void setup() {
 
 void loop() {
   modes[selected_mode].loop();
+
+  // Try to connect Wi-Fi
+  if ((WiFi.status() != WL_CONNECTED) && (millis() - lastWifiConnectionAttempt > WIFI_CONNECTING_INTERVAL))
+  {
+    lastWifiConnectionAttempt = millis();
+    setupWifi();
+  }
+
+  mqttClient.loop();
 }
---
+++ src/Firmware/Weighbridge/mqtt.h
@@ -0,0 +1,33 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#include "config.h"
+#include "logging.h"
+
+#ifndef MQTT_H
+#define MQTT_H
+
+WiFiClient wifiClient = WiFiClient();
+PubSubClient mqttClient = PubSubClient(wifiClient);
+
+int publish(const char* channel, const char* message)
+{
+  mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
+
+  // Connect MQTT broker
+  while (!mqttClient.connected())
+  {
+    DEBUG_PRINTF("publish(): Connecting to MQTT broker '%s: %i'...\n", MQTT_SERVER, MQTT_PORT);
+
+    if (!mqttClient.connect(MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD))
+    {
+      DEBUG_PRINTF("publish(): Connection failed with error code %i\n", mqttClient.state());
+      return mqttClient.state();
+    }
+  }
+
+  DEBUG_PRINTF("publish(): Publishing message on channel '%s': %s\n", channel, message);
+  return mqttClient.publish(channel, message);
+}
+
+#endif //MQTT_H
